ff7b21820afafaf9d6e9ff72e1af780fdbf15cd6
Suresh Injeti <sureshkumar.injeti@hcl.com>
2025-10-24 23:25:28 +0530

    APi Gateway properties updated

--- ek8s-deploy.sh
+++ ek8s-deploy.sh
@@ -6,12 +6,16 @@ echo "Step-1 ==== Building Docker images ===="
 docker build -t product-service:latest ./productservice
 docker build -t order-service:latest ./orderservice
 docker build -t inventory-service:latest ./inventoryservice
+docker build -t customerservice:latest ./customerservice
+docker build -t notificationservice:latest ./notificationservice
 docker build -t api-gateway:latest ./apigateway
 
 echo "Step-2 ==== Loading images into Minikube ===="
 minikube image load product-service:latest
 minikube image load order-service:latest
 minikube image load inventory-service:latest
+minikube image load customerservice:latest
+minikube image load notificationservice:latest
 minikube image load api-gateway:latest
 
 echo "Step-3 ==== Applying Kubernetes manifests ===="
@@ -32,11 +36,19 @@ kubectl create configmap order-db-init \
 kubectl create configmap inventory-db-init \
   --from-file=init.sql=./inventoryservice/mysql/init.sql \
   -n microservices
+
+kubectl create configmap customer-db-init \
+  --from-file=init.sql=./customerservice/mysql/init.sql \
+  -n microservices
+  
+  
 echo "Step-3.3 ==== Deploy k8smanifest file ===="
 # Step 3.3: Deploy all manifests
 kubectl apply -f k8s/product-service-manifests.yaml
 kubectl apply -f k8s/order-service-manifests.yaml
 kubectl apply -f k8s/inventory-service-manifests.yaml
+kubectl apply -f k8s/customer-service-manifests.yaml
+kubectl apply -f k8s/notification-service-manifests.yaml
 kubectl apply -f k8s/api-gateway-manifests.yaml
 
 echo "==== Deployment complete! ===="
